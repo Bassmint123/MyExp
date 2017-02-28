07b64a786f0a1fddaad668236b155b8a0ec98d9b
Bassmint123 <bradb@cimstrat.com>
2017-02-28 10:57:23 -0600

    Added new note capability

--- app/controllers/mynotes_controller.rb
+++ app/controllers/mynotes_controller.rb
@@ -4,4 +4,23 @@ class MynotesController < ApplicationController
     @mynotes = Mynote.all
   end
 
+  def new 
+    @mynotes = Mynote.new 
+  end
+
+  def create 
+    @mynote = Mynote.new(message_params) 
+    if @mynote.save 
+      redirect_to '/mynotes' 
+    else 
+      render 'new' 
+    end 
+  end
+
+
+  private 
+  def message_params 
+    params.require(:mynote).permit(:content) 
+  end
+
 end
